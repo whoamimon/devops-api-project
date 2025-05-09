2dd7f0955934482b3c229be9f1b502ce9e4f68dc
whoamimon <pomaquero@proton.me>
2025-05-09 17:27:42 -0500

    added iam user for cicd

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -3,11 +3,11 @@
 #############################################################
 
 resource "aws_iam_user" "cd" {
-    name = "recipe-app-api-cd"
+  name = "recipe-app-api-cd"
 }
 
 resource "aws_iam_access_key" "cd" {
-  user = aws_iam_user.cd
+  user = aws_iam_user.cd.name
 }
 
 ###########################################################
@@ -16,37 +16,37 @@ resource "aws_iam_access_key" "cd" {
 
 data "aws_iam_policy_document" "tf_backend" {
   statement {
-    effect = "Allow"
-    actions = [ "s3:ListBucket" ]
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
     resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
   }
   statement {
-    effect = "Allow"
-    actions = [ "s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+    effect  = "Allow"
+    actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
     resources = [
-        "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
-        "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*"
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*"
     ]
   }
   statement {
     effect = "Allow"
-    actions = [ 
-        "dynamodb:DescribeTable",
-        "dynamodb:GetItem",
-        "dynamodb:PutItem",
-        "dynamodb:DeleteItem"
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
     ]
-    resources = "arn:aws:dynamodb:*:*:table/${var.tf_state_lock_table}"
+    resources = ["arn:aws:dynamodb:*:*:table/${var.tf_state_lock_table}"]
   }
 }
 
 resource "aws_iam_policy" "tf_backend" {
-  name = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
+  name        = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
   description = "Allow user to use S3 and dynamodb for TF backend resources"
-  policy = data.aws_iam_policy_document.tf_backend.json
+  policy      = data.aws_iam_policy_document.tf_backend.json
 }
 
 resource "aws_iam_user_policy_attachment" "tf_backend" {
-  user = aws_iam_user.cd.name
+  user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.tf_backend.arn
 }
